3423f1a1e06925713a3f99fb5c01678004defa55
Israel Lohuan <israel@globalsistemas.com.br>
2021-02-15 18:42:46 -0300

    Finalizado projeto

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -70,11 +70,25 @@ class EventController extends Controller
     {
         $event = Event::findOrFail($id);
 
+        $user = auth()->user();
+        $hasUserJoined = false;
+
+        if($user) {
+            $userEvents = $user->eventAsParticipant->toArray();
+           
+            foreach($userEvents as $userEvent) {
+                if($userEvent["id"] == $id) {
+                    $hasUserJoined = true;
+                }
+            }
+        }
+
         $eventOwner = User::where("id", $event->user_id)->first()->toArray();
 
         return view("events.show", [
             "event" => $event,
-            "eventOwner" => $eventOwner
+            "eventOwner" => $eventOwner,
+            "hasUserJoined" => $hasUserJoined
         ]);
     }
 
@@ -144,5 +158,16 @@ class EventController extends Controller
         $event = Event::findOrFail($id);
 
         return redirect("/dashboard")->with("msg", "Sua presença está confirmada no evento! " . $event->title);
+    }
+
+    public function leaveEvent($id)
+    {
+        $user = auth()->user();
+
+        $user->eventAsParticipant()->detach($id);
+
+        $event = Event::findOrFail($id);
+
+        return redirect("/dashboard")->with("msg", "Sua presença foi removida do evento! " . $event->title);
     }
 }
